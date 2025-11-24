ddc6696843323c460e1e558d60400b0ed6842e24
Kevin-Olazo <kevind.olazo@gmail.com>
2025-11-24 15:59:15 -0500

    Actualizado ProductoServlet para que envie datos de entradaProducto a salidaProducto

--- src/main/java/controladores/ProductoServlet.java
+++ src/main/java/controladores/ProductoServlet.java
@@ -1,4 +1,32 @@
 package controladores;
 
-public class ProductoServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/RegistrarProducto")
+public class ProductoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String nombre = req.getParameter("txtNombreProducto");
+        double precio = Double.parseDouble(req.getParameter("txtPrecioProducto"));
+        int cantidad = Integer.parseInt(req.getParameter("txtCantidadProducto"));
+
+        req.setAttribute("nombre", nombre);
+        req.setAttribute("precio", precio);
+        req.setAttribute("cantidad", cantidad);
+
+        req.getRequestDispatcher("salidaProducto.jsp").forward(req,resp);
+
+    }
 }
